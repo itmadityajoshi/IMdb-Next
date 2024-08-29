c8c2ec1b99069541fba7ca6f76587c6687557653
itmadityajoshi <adityajoshi316@gmail.com>
2024-08-29 12:13:55 +0545

    add about section

---
+++ src/app/about/page.jsx
@@ -0,0 +1,36 @@
+const page = () => {
+  return (
+  <>
+   <div className="max-w-6xl mx-auto p-3 space-y-4" >
+    <h1 className="text-2xl font-medium text-orange-600 mb-4">About</h1>
+    
+    <p>
+
+        Since 1990, an incredibly diverse range of people across the globe have been adding, refining, and correcting the data on IMDb. It's an ever-evolving process and we invite you to become a part of it. To become a contributor, you first need to register, but don’t worry it’s free and painless. After registering, check out the Getting Started guide to start submitting. Thank you for your interest and we appreciate your help making IMDb the world’s authoritative source for movie, TV, and celebrity content.
+    </p>
+    <p>
+     <span className="text-orange-600  text-xl"> What is IMDb?</span> <br />
+
+Launched online in 1990 and a subsidiary of Amazon.com since 1998, IMDb is the world's most popular and authoritative source for movie, TV and celebrity content, designed to help fans explore the world of movies and shows and decide what to watch.
+
+Our searchable database includes millions of movies, TV and entertainment programs and cast and crew members. 
+
+IMDb can help you:
+
+Jog your memory about a movie, show, or person on the tip of your tongue
+Find the best movie or show to watch next
+Empower you to share your entertainment knowledge and opinions with the world’s largest community of fans
+For fans deciding what to watch and where to watch it, we offer local movie showtimes, ticketing, trailers, critic and user reviews, personalized recommendations, photo galleries, entertainment news, quotes, trivia, box-office data, editorial feature sections and a universal Watchlist. To learn more about watching Trailers, Clips, Featurettes, and IMDb Originals, please see the IMDb Video FAQs.
+
+IMDb creates new original video content each week and you can find it all at IMDb Originals.
+
+You can find new videos highlighted every day on the IMDb homepage via your desktop computer or IMDb app.
+
+
+    </p>
+   </div>
+  </>
+  )
+};
+
+export default page;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,18 +2,24 @@ import Link from "next/link";
 import MenuItem from "./MenuItem";
 import { AiFillHome } from "react-icons/ai";
 import { BsFillInfoCircleFill } from "react-icons/bs";
+import DarkMode from "./DarkMode";
 
 function Header() {
   return (
-    <div className="flex justify-between p-5 items-center bg-slate-400 mx-auto max-w-6xl">
+    <div className="flex justify-between p-5 items-center 0 mx-auto max-w-6xl">
       <div className="flex gap-5">
         <MenuItem title="home" address="/" Icon={AiFillHome} />
         <MenuItem title="about" address="/about" Icon={BsFillInfoCircleFill} />
       </div>
+
+      <div className="flex items-center gap-4">
+        <DarkMode />
       <Link href={"/"} className="flex gap-1 items-center">
         <span className="font-bold bg-orange-600 p-2 rounded-lg ">IMDB</span>
         <span className="hidden sm:inline font-semibold">clone</span>
       </Link>
+      </div>
+
     </div>
   );
 }
