69c59110383c1b4bd43c62f38787334a526b1dfd
itmadityajoshi <adityajoshi316@gmail.com>
2024-08-29 12:49:28 +0545

    create list of items present in Navbar

---
+++ src/components/NavbarItem.jsx
@@ -0,0 +1,22 @@
+'use client'
+import Link from "next/link"
+import { useSearchParams } from "next/navigation"
+
+
+function NavbarItem({title, param}) {
+
+        const searchParams = useSearchParams()
+
+        const genre = searchParams.get('genre')
+
+  return (
+    <div className="">
+        <Link 
+        className={`hover:text-orange-600  font-semibold ${genre === param ? 'underline underline-offset-8  decoration-orange-600 rounded-xl':''}`}
+        href={`/?genre=${param}`}
+        >{title}</Link>
+    </div>
+  )
+}
+
+export default NavbarItem 
